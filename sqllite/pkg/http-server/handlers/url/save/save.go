33bfa510986de9fa5f37e0ef7f1b19f4d5b95b39
NikitaLRMT <nikita.lrmt1@gmail.com>
2025-05-05 03:34:17 +0300

    Добавлен проект сокращения URL и README файл

---
+++ sqllite/pkg/http-server/handlers/url/save/save.go
@@ -0,0 +1,95 @@
+package save
+
+import (
+	"errors"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
+	"log/slog"
+	"net/http"
+	"sqllite/pkg/lib/api/response"
+	"sqllite/pkg/lib/logger/sl"
+	"sqllite/pkg/lib/random"
+	"sqllite/pkg/storage"
+)
+
+const aliasLength = 6
+
+type Request struct {
+	URL   string `json:"url" validate:"required, url"`
+	Alias string `json:"alias,omitempty"`
+}
+
+type Response struct {
+	response.Response
+	Alias string `json:"alias,omitempty"`
+}
+
+type URLSaver interface {
+	SaveURL(urlToSave string, alias string) (int64, error)
+}
+
+func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.url.save"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req Request
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode request body", sl.Err(err))
+
+			render.JSON(w, r, response.Error("failed to decode request body"))
+
+			return
+		}
+
+		log.Info("request body decoded", slog.Any("request", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			validateErr := err.(validator.ValidationErrors)
+			log.Error("invalid request", sl.Err(err))
+
+			render.JSON(w, r, response.ValidationError(validateErr))
+
+			return
+		}
+
+		alias := req.Alias
+		if alias == "" {
+			alias = random.NewRandomString(aliasLength)
+		}
+
+		id, err := urlSaver.SaveURL(req.URL, alias)
+		if errors.Is(err, storage.ErrURLExists) {
+			log.Info("url already exists", slog.String("url", req.URL))
+
+			render.JSON(w, r, response.Error("url already exists"))
+
+			return
+		}
+		if err != nil {
+			log.Error("failed to save url", sl.Err(err))
+
+			render.JSON(w, r, response.Error("failed to save url"))
+
+			return
+		}
+
+		log.Info("url saved", slog.Int64("id", id))
+
+		responseOK(w, r, alias)
+
+	}
+}
+
+func responseOK(w http.ResponseWriter, r *http.Request, alias string) {
+	render.JSON(w, r, Response{
+		Response: response.OK(),
+		Alias:    alias,
+	})
+}
